2d26eb902755507904903e0612fa01d1b7161b29
Alejandro <alejandroarribascantero@gmail.com>
2025-12-07 18:07:13 +0100

    s

--- mvp_vote/wsgi.py
+++ mvp_vote/wsgi.py
@@ -5,3 +5,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mvp_vote.settings.prod')
 
 application = get_wsgi_application()
+
+app = application
